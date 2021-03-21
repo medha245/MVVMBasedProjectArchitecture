9d286549a85b3c90ad5e3bc2990d352f150a7e0b
medha742 <medha.singh@kartrocket.com>
2021-03-22 00:03:33 +0530

    resolved a crash

--- app/src/main/java/com/medha/myapplication/MyApplication.kt
+++ app/src/main/java/com/medha/myapplication/MyApplication.kt
@@ -1,10 +1,19 @@
 package com.medha.myapplication
 
+import android.app.Application
+import com.medha.myapplication.utilities.SharedPreferencesHelper
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class MyApplication {
+class MyApplication : Application(){
     interface InvalidateUnAuthorizedSession {
         fun invalidate()
     }
+
+    override fun onCreate() {
+        super.onCreate()
+        SharedPreferencesHelper.context = this
+    }
+
+
 }
